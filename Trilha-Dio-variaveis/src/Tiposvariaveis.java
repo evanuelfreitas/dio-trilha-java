03a9ed70056a2cba7fc4c52785a75e6c0c3d39e2
evanuelfreitas <evanuel.freitas@hotmail.com>
2023-01-25 15:36:48 -0300

    tipos de variaveis primitivos

--- Trilha-Dio-variaveis/src/Tiposvariaveis.java
+++ Trilha-Dio-variaveis/src/Tiposvariaveis.java
@@ -1,5 +1,20 @@
 public class Tiposvariaveis {
     public static void main(String[] args) throws Exception {
-        
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+
+
+        System.out.println(numeroCurto);
+        System.out.println(numeroNormal);
+        System.out.println(numeroCurto2);
+
+        final int NUMERO_INTEIRO = 5;
+
+        System.out.println(NUMERO_INTEIRO);
+
     }
 }
