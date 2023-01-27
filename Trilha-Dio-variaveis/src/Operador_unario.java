b537276a7649ac673a4b99f318b686ea8a46c37f
evanuelfreitas <evanuel.freitas@hotmail.com>
2023-01-27 06:54:05 -0300

    teste operador unario

---
+++ Trilha-Dio-variaveis/src/Operador_unario.java
@@ -0,0 +1,12 @@
+public class Operador_unario {
+    public static void main(String[] args) {
+        int numero = 5;
+        numero = -numero;
+        System.out.println(numero);
+        System.out.println(-numero);
+        System.out.println(- numero);
+
+        numero = numero + 1;
+        System.out.println(numero);
+    }
+}
