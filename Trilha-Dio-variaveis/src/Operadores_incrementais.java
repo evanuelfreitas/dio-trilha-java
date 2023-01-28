8fa935b73dd1574b5604d4405b7bb4169071ac73
evanuelfreitas <evanuel.freitas@hotmail.com>
2023-01-28 07:42:42 -0300

    Renomeação de arquivo

---
+++ Trilha-Dio-variaveis/src/Operadores_incrementais.java
@@ -0,0 +1,14 @@
+public class Operadores_incrementais {
+    public static void main(String[] args) {
+        int numero = 5;
+        boolean valida = false;
+        System.out.println(numero);
+        System.out.println(-numero);
+        System.out.println(- numero);
+        System.out.println(valida);
+        System.out.println(!valida);
+        
+        
+        System.out.println(numero);
+    }
+}
