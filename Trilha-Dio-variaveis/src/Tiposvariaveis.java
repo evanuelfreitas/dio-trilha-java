f36bf0b87f04f29628c4e2d8e3a4e48efd625b9b
Evanuel Freitas <evanuel.freitas@hotmail.com>
2023-01-28 12:17:17 -0300

    Teste nova instalação

--- Trilha-Dio-variaveis/src/Tiposvariaveis.java
+++ Trilha-Dio-variaveis/src/Tiposvariaveis.java
@@ -12,7 +12,7 @@ public class Tiposvariaveis {
         System.out.println(numeroNormal);
         System.out.println(numeroCurto2);
 
-        final int NUMERO_INTEIRO = 5;
+        final int NUMERO_INTEIRO = 6;
 
         System.out.println(NUMERO_INTEIRO);
 
