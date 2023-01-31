2241f5bc4ce81a563f00a52c3bab91399c1b9df3
Evanuel Freitas <evanuel.freitas@hotmail.com>
2023-01-31 07:00:06 -0300

    Testes com operadores logicos

---
+++ Trilha-Dio-variaveis/src/Operadores_Logicos.java
@@ -0,0 +1,14 @@
+public class Operadores_Logicos {
+    public static void main(String[] args) {
+        boolean condicao1, condicao2;
+        condicao1 = true;
+        condicao2 = true;
+
+        if (condicao1 && condicao2) {
+            System.out.println("As duas expressoes sao verdadeiras");
+        } else {
+            System.out.println("Apenas uma ou nenhuma é verdadeira");
+        }
+
+    }
+}
