712058b8d73877faad9aeea35cfe3f4a3a17bff0
Evanuel Freitas <evanuel.freitas@hotmail.com>
2023-02-16 07:16:27 -0300

    Testes de código

--- Terminal-e-Argumentos/TerminalEArgumentos/src/App.java
+++ Terminal-e-Argumentos/TerminalEArgumentos/src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner captura = new Scanner(System.in);
+        System.out.println("Digite um numero");
+        int numero1 = captura.nextInt();
+        System.out.println(numero1);
+        String numero2 = "2";
+        System.out.println(numero1+numero2);
     }
 }
