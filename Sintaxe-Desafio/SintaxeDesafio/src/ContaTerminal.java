d1292bb16862f15c9075cf2e2535095d817840f7
Evanuel Freitas <evanuel.freitas@hotmail.com>
2023-02-17 07:28:18 -0300

    Implementando as funções solicitadas

--- Sintaxe-Desafio/SintaxeDesafio/src/ContaTerminal.java
+++ Sintaxe-Desafio/SintaxeDesafio/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroConta;
+        String numeroAgencia;
+        String nomeCliente;
+        double saldoConta;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Olá! Por favor digite sua conta");
+        numeroConta = scan.nextInt();
+        System.out.println("Agora digite o número da agência com dígito");
+        numeroAgencia = scan.next();
+        System.out.println("Seu nome por favor");
+        nomeCliente = scan.next();
+        System.out.println("O saldo agora");
+        saldoConta = scan.nextDouble();
+
+        System.out.println("Obrigado, sr.(a) " + nomeCliente +  ". O numero de sua conta é " + numeroConta);
+        System.out.println("O Numero da Agencia é " + numeroAgencia);
+        System.out.println("Seu saldo disponivel é de R$"+saldoConta);
+        System.out.println("Nos vemos em breve!");
+    
+        
+
+
     }
 }
