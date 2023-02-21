0370ce7e4b02cd6f9b56bf0797729fa746fd84b7
Evanuel Freitas <evanuel.freitas@hotmail.com>
2023-02-21 07:29:08 -0300

    Testes com controle de fluxo e condicionais

---
+++ Controle-de-Fluxo/Controle-Fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ResultadoEscolar {
+        public static void main(String[] args) {
+        
+        Scanner scanNota = new Scanner (System.in);
+        System.out.println("Por favor digite a nota do aluno");
+        double nota = scanNota.nextDouble();
+        
+        /* if (nota>=7){
+        
+           System.out.println("Aprovado");
+        } 
+            else if (nota>=5 && nota<7){
+            System.out.println("Prova de recuperação");
+            }
+            else {
+            System.out.println("Reprovado");
+        }
+
+        */ 
+        //Mesmo propósito do if, mas utilizando operador ternário
+
+        String resultado = nota >= 7 ? "Aprovado": nota >=5 && nota< 7 ? "Recuperação":"Reprovado";
+        System.out.println(resultado);
+    }
+}
---
+++ Controle-de-Fluxo/Controle-Fluxo/src/SistemaMedida.java
@@ -0,0 +1,23 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "P";
+        switch (sigla){
+            case "P":{
+                System.out.println("Pequeno");
+                break;
+            }
+            case "M":{
+                System.out.println("Médio");
+                break;
+            }
+            case "G":{
+                System.out.println("Grande");
+                break;
+            }
+            default: {
+                System.out.println("Indefinido ou fora de padrão");
+                break;
+            }
+        }
+    }
+}
