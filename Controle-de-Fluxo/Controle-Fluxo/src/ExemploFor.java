639de03c04622e3315aea70c7e591e852f6d519d
Evanuel Freitas <evanuel.freitas@hotmail.com>
2023-02-22 06:31:40 -0300

    Testes de execução laço for

---
+++ Controle-de-Fluxo/Controle-Fluxo/src/ExemploFor.java
@@ -0,0 +1,13 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int carneirinhos = 0; carneirinhos <= 20; carneirinhos++){
+            System.out.println(carneirinhos);
+        }
+        int ponto = 0;
+        for (int contador=1;contador<=3000;contador++){
+            ponto = ponto+1;
+            System.out.println(ponto);
+        }  
+    }
+    
+}
